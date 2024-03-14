a7ab5eb5fd11d57cb5e40715b539b9a29a6fd8fc
artiehumphreys <artieh2003@gmail.com>
2024-03-13 22:12:41 -0400

    Home screen now redirects to login page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Loginpage from './pages/Loginpage';
 
-function App() {
+//TODO: Implement login logic
+//Use protected routes.
+const isAuthenticated = () => {
+  return false;
+};
+
+const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<Loginpage />} />
+        <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
     </Router>
   );
-}
+};
 
 export default App;
