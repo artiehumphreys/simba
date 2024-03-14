fb7b39111a54f7afdd32456ca45c7cfc1b204069
artiehumphreys <artieh2003@gmail.com>
2024-03-13 23:40:50 -0400

    Added logo and changed color scheme.

--- src/pages/Loginpage.js
+++ src/pages/Loginpage.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import LoginForm from '../components/LoginForm';
 import styles from './Loginpage.module.css';
+const logoPath = process.env.PUBLIC_URL + '/simba_logo.png';
+//Logo is (0, 140, 180)
 
 function LoginPage() {
     return (
@@ -8,6 +10,8 @@ function LoginPage() {
             {/* <h2>Login to Your Account</h2> */}
             <div style={{ background: '#121212', minHeight: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
                 <div className={styles.formContainer}>
+                    <img src={logoPath} alt="Simba Logo">
+                    </img>
                     <LoginForm />
                 </div>
             </div>
