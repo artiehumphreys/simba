cdfa23b2d78acc5192cc77a9ede3ae71c6d6af6d
artiehumphreys <artieh2003@gmail.com>
2024-03-25 14:43:41 -0400

    Working on fetching playlists as a proof of concept

--- backend/go-service/pkg/spotifyAPI/client.go
+++ backend/go-service/pkg/spotifyAPI/client.go
@@ -1,19 +1,37 @@
 package main
 
 import (
+	"context"
 	"log"
 	"os"
 
 	"github.com/joho/godotenv"
+	"github.com/zmb3/spotify/v2"
+	spotifyauth "github.com/zmb3/spotify/v2/auth"
+	"golang.org/x/oauth2/clientcredentials"
 )
 
 func main() {
-	// Load .env file at the start of your application
 	if err := godotenv.Load(); err != nil {
 		log.Fatalf("Error loading .env file: %v", err)
 	}
-
-	// Now you can use os.Getenv to access environment variables anywhere in your app
-	clientID := os.Getenv("SPOTIFY_CLIENT_ID")
-	clientSecret := os.Getenv("SPOTIFY_CLIENT_SECRET")
+	authConfig := &clientcredentials.Config{
+		ClientID:     os.Getenv("SPOTIFY_CLIENT_ID"),
+		ClientSecret: os.Getenv("SPOTIFY_CLIENT_SECRET"),
+		TokenURL:     spotifyauth.TokenURL,
+	}
+	token, err := authConfig.Token(context.Background())
+	if err != nil {
+		log.Fatalf("Couldn't get token: %v", err)
+	}
+	httpClient := spotifyauth.New().Client(context.Background(), token)
+	client := spotify.New(httpClient)
+	playlistID := spotify.ID("37i9dQZF1DXcBWIGoYBM5M")
+	playlist, err := client.GetPlaylist(context.Background(), playlistID)
+	if err != nil {
+		log.Fatalf("error retrieving playlist data: %v", err)
+	}
+	log.Println("playlist id:", playlist.ID)
+	log.Println("playlist name:", playlist.Name)
+	log.Println("playlist description:", playlist.Description)
 }
--- backend/go.mod
+++ backend/go.mod
@@ -3,11 +3,11 @@ module simba
 go 1.22.1
 
 require (
-	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/zmb3/spotify/v2 v2.4.1 // indirect
-	golang.org/x/net v0.17.0 // indirect
-	golang.org/x/oauth2 v0.0.0-20210810183815-faf39c7919d5 // indirect
+	golang.org/x/net v0.22.0 // indirect
+	golang.org/x/oauth2 v0.18.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
-	google.golang.org/protobuf v1.27.1 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
 )
