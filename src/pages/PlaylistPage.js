17076d508e426f2a5cea4ec6c4b16d6a59586bad
artiehumphreys <artieh2003@gmail.com>
2024-05-13 19:56:01 -0400

    Need to make fetch tracks a hook

--- src/pages/PlaylistPage.js
+++ src/pages/PlaylistPage.js
@@ -2,12 +2,14 @@ import React, { useState, useEffect } from 'react';
 import Footer from '../components/Footer';
 import SideBar from '../components/SideBar';
 import styles from '../css/HomePage.module.css';
+import { useParams } from 'react-router-dom';
+
 
 function fetchTracks(playlistId) {
     fetch(`http://localhost:8080/api/playlists/${playlistId}/songs`)
         .then(response => response.json())
         .then(data => {
-            console.log(data);
+            return data
         })
         .catch(error => {
             console.error('Error fetching tracks:', error);
@@ -15,6 +17,8 @@ function fetchTracks(playlistId) {
 }
 
 function PlaylistPage(){
+    const { id } = useParams();
+    const tracks = fetchTracks(id);
     return(
         <body className={`flex h-screen text-white overflow-hidden ${styles.background_container}`}>
             <aside className={`w-1/6 flex flex-col items-center px-4 py-8 bg-gray-900`}>
@@ -22,9 +26,11 @@ function PlaylistPage(){
             </aside>
             <div className="flex-grow w-10/12 p-8 overflow-y-auto mb-20">
                 <div className="grid grid-rows-1 gap-4">
-                    <div className="bg-gray-800 p-4 rounded-lg cursor-pointer transform transition duration-200 hover:scale-105 w-full md:w-auto">
-                        <h3 className="mt-2 text-center text-white">hi</h3>
+                {tracks && tracks.map((track, index) => (
+                    <div key={index} className="bg-gray-800 p-4 rounded-lg cursor-pointer transform transition duration-200 hover:scale-105 w-full md:w-auto">
+                        <h3 className="mt-2 text-center text-white">{track.name}</h3>
                     </div>
+                ))}
                 </div>
         </div>
 
