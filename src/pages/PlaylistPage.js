8136207e8be1220aa43bb3b45ac50c7414ad4287
artiehumphreys <artieh2003@gmail.com>
2024-05-13 21:29:40 -0400

    Made it into a hook

--- src/pages/PlaylistPage.js
+++ src/pages/PlaylistPage.js
@@ -4,27 +4,28 @@ import SideBar from '../components/SideBar';
 import styles from '../css/HomePage.module.css';
 import { useParams } from 'react-router-dom';
 
-
-function fetchTracks(playlistId) {
-    fetch(`http://localhost:8080/api/playlists/${playlistId}/songs`)
-        .then(response => response.json())
-        .then(data => {
-            return data
-        })
-        .catch(error => {
-            console.error('Error fetching tracks:', error);
-        });
+function useFetchTracks(playlistId) {
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
+    useEffect(() => {
+        fetch(`http://localhost:8080/api/playlists/${playlistId}/songs`)
+            .then(response => response.json())
+            .then(setData)
+            .catch(setError);
+        }, [playlistId]);
+        return { data, error };
 }
 
 function PlaylistPage(){
     const { id } = useParams();
-    const tracks = fetchTracks(id);
+    const { data: tracks, error } = useFetchTracks(id);
     return(
         <body className={`flex h-screen text-white overflow-hidden ${styles.background_container}`}>
             <aside className={`w-1/6 flex flex-col items-center px-4 py-8 bg-gray-900`}>
                 <SideBar></SideBar>
             </aside>
             <div className="flex-grow w-10/12 p-8 overflow-y-auto mb-20">
+            {error && <div className="text-red-500">Error fetching tracks: {error.message}</div>}
                 <div className="grid grid-rows-1 gap-4">
                 {tracks && tracks.map((track, index) => (
                     <div key={index} className="bg-gray-800 p-4 rounded-lg cursor-pointer transform transition duration-200 hover:scale-105 w-full md:w-auto">
