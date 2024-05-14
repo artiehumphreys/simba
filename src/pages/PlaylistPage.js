2c7b82d5f20f07fd53bcaec9bd04716037f97047
artiehumphreys <artieh2003@gmail.com>
2024-05-14 12:37:58 -0400

    Added album and calculated minutes and seconds fromms. Added images next to title

--- src/pages/PlaylistPage.js
+++ src/pages/PlaylistPage.js
@@ -27,17 +27,30 @@ function PlaylistPage(){
             <div className="flex-grow w-10/12 p-8 overflow-y-auto mb-20">
             {error && <div className="text-red-500">Error fetching tracks: {error.message}</div>}
 
-                <div className="grid grid-rows-1 gap-4">
-                {tracks && tracks.map((track, index) => (
-                    <div key={index} className="bg-gray-800 p-4 rounded-lg cursor-pointer transform transition duration-200 hover:scale-105 w-full md:w-auto">
-                        <h3 className="mt-2 text-left text-white">{track.name}</h3>
-                        <span className='text-white'>
-                            {track.artists && track.artists.map(artist => artist.name).join(', ')}
+            <div className="grid grid-rows-1 gap-2">
+            {tracks && tracks.map((track, index) => (
+                <div key={index} className="bg-gray-800 p-4 rounded-lg cursor-pointer transform transition duration-200 hover:scale-105 flex items-center justify-between w-full md:w-auto">
+                    <div className="flex items-center space-x-4">
+                        <img src={track.album.images[0].url} alt={track.name}
+                            className="w-12 h-12 object-cover rounded-lg" />
+                        <div className="flex flex-col">
+                            <h3 className="text-white text-lg leading-tight">{track.name}</h3>
+                            <span className="text-slate-500 text-sm">
+                                {track.artists && track.artists.map(artist => artist.name).join(', ')}
+                            </span>
+                        </div>
+                    </div>
+                    <div className="flex items-center">
+                        <span className="text-slate-500 text-s mr-10">{track.album && track.album.name}</span>
+                        <span className="text-slate-500 text-s">
+                            {Math.floor(track.duration_ms / 60000)}:{(Math.floor((track.duration_ms % 60000) / 1000)).toString().padStart(2, '0')}
                         </span>
                     </div>
-                ))}
                 </div>
+            ))}
         </div>
+
+            </div>
 
             <Footer></Footer>
         </body>
