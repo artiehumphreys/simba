47043a5185ff6fb4b88fce01801e38ac0fcb739d
artiehumphreys <artieh2003@gmail.com>
2024-05-16 17:22:09 -0400

    Playing preview audio now works on click. Will do spotify OAuth later.

--- src/pages/PlaylistPage.js
+++ src/pages/PlaylistPage.js
@@ -4,42 +4,82 @@ import SideBar from '../components/SideBar';
 import styles from '../css/HomePage.module.css';
 import { useParams } from 'react-router-dom';
 
-function useFetchTracks(playlistId) {
+function useFetchTracks(endpoint) {
     const [data, setData] = useState(null);
     const [error, setError] = useState(null);
     useEffect(() => {
-        fetch(`http://localhost:8080/api/playlists/${playlistId}/tracks`)
+        fetch(`http://localhost:8080/${endpoint}`)
             .then(response => response.json())
             .then(setData)
             .catch(setError);
-        }, [playlistId]);
+        }, [endpoint]);
         return { data, error };
 }
 // https://github.com/spotify/spotify-web-playback-sdk-example/blob/main/src/WebPlayback.jsx
-function useSpotifySDK(trackId){
-    const [is_paused, setPaused] = useState(false);
-    const [is_active, setActive] = useState(false);
-    const [player, setPlayer] = useState(null);
-    const [current_track, setTrack] = useState(null);
-    useEffect(() => {
+// function useWebPlayback(trackId){
+//     const [is_paused, setPaused] = useState(false);
+//     const [is_active, setActive] = useState(false);
+//     const [player, setPlayer] = useState(null);
+//     const [current_track, setTrack] = useState(null);
+//     useEffect(() => {
 
-        const script = document.createElement("script");
-        script.src = "https://sdk.scdn.co/spotify-player.js";
-        script.async = true;
+//         const script = document.createElement("script");
+//         script.src = "https://sdk.scdn.co/spotify-player.js";
+//         script.async = true;
 
-        document.body.appendChild(script);
+//         document.body.appendChild(script);
 
-        window.onSpotifyWebPlaybackSDKReady = () => {
-            
-        };
+//         window.onSpotifyWebPlaybackSDKReady = () => {
+//             const player = new window.Spotify.Player({
+//                 name: 'Web Playback SDK',
+//                 getOAuthToken: cb => { cb(props.token); },
+//                 volume: 0.5
+//             });
 
-    }, []);
+//             setPlayer(player);
 
-}
+//             player.addListener('ready', ({ device_id }) => {
+//                 console.log('Ready with Device ID', device_id);
+//             });
+
+//             player.addListener('not_ready', ({ device_id }) => {
+//                 console.log('Device ID has gone offline', device_id);
+//             });
+
+//             player.addListener('player_state_changed', ( state => {
+
+//                 if (!state) {
+//                     return;
+//                 }
+
+//                 setTrack(state.track_window.current_track);
+//                 setPaused(state.paused);
+
+//                 player.getCurrentState().then( state => { 
+//                     (!state)? setActive(false) : setActive(true) 
+//                 });
+
+//             }));
+
+//             player.connect();
+
+
+//         };
+
+//     }, []);
+
+// }
+
+function playPreview(trackId) {
+    const audio = document.getElementById(trackId);
+    if (audio) {
+      audio.play();
+    }
+  }
 
 function PlaylistPage(){
     const { id } = useParams();
-    const { data: tracks, error } = useFetchTracks(`'api/playlists/${id}/tracks'`);
+    const { data: tracks, error } = useFetchTracks(`api/playlists/${id}/tracks`);
     return(
         <body className={`flex h-screen text-white overflow-hidden ${styles.background_container}`}>
             <aside className={`w-1/6 flex flex-col items-center px-4 py-8 bg-gray-900`}>
@@ -50,7 +90,7 @@ function PlaylistPage(){
 
             <div className="grid grid-rows-1 gap-2">
             {tracks && tracks.map((track, index) => (
-                <div key={index} className="bg-gray-800 p-4 rounded-lg cursor-pointer transform transition duration-200 hover:scale-105 flex items-center justify-between w-full md:w-auto">
+                <div key={index} className="bg-gray-800 p-4 rounded-lg cursor-pointer transform transition duration-200 hover:scale-105 flex items-center justify-between w-full md:w-auto" onClick={() => playPreview(`audio-${index}`)}>
                     <div className="flex items-center space-x-4">
                         <img src={track.album.images[0].url} alt={track.album.name}
                             className="w-12 h-12 object-cover rounded-lg" />
@@ -66,6 +106,9 @@ function PlaylistPage(){
                         <span className="text-slate-500 text-s">
                             {Math.floor(track.duration_ms / 60000)}:{(Math.floor((track.duration_ms % 60000) / 1000)).toString().padStart(2, '0')}
                         </span>
+                        <audio id={`audio-${index}`}>
+                            <source src={track.preview_url} type="audio/mpeg"></source>
+                        </audio>
                     </div>
                 </div>
             ))}
